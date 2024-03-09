875b4fa36415fe2417ed9c01f259a1ae57df0892
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-09 12:45:55 +0530

    4th commit

--- BasicsofNodejs/intro.js
+++ BasicsofNodejs/intro.js
@@ -15,5 +15,17 @@
 // }, 1000)
 
 // Modules
+const john = 'john'
+const peter = 'peter'
+const sayHi = (name) =>{
+console.log(`hello there ${name}`)
+}
+sayHi('susan');
+sayHi(john);
+sayHi(peter);
+
+
+
+
 
 
