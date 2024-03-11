4f74893d3e5d8e58d01f8c1f8fceef83a5de9fd7
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-11 16:26:19 +0530

    Commited

---
+++ BasicsofNodejs/fs-synce.js
@@ -0,0 +1,12 @@
+const { isUtf8 } = require("buffer");
+const { readFileSync, writeFileSync } = require("fs");
+
+const first = readFileSync("./content/first.txt", "utf-8");
+console.log(first);
+const second = readFileSync("./content/subfolder/second.txt", "utf-8");
+console.log(second);
+
+writeFileSync(
+  "./content/subfolder/result.txt",
+  `This is the result : ${first} , ${second}`
+);
