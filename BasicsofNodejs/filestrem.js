528b5b70314980e911dc226d35943953ca5d849e
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2024-03-14 13:07:36 +0530

    commited

---
+++ BasicsofNodejs/filestrem.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http
+  .createServer((req, res) => {
+    // const text = readFileSync('./BasicsofNodejs/content/bigFile.txt', 'utf8');
+    // res.end(text)
+    const fileStream = fs.createReadStream(
+      "./BasicsofNodejs/content/bigFile.txt",
+      "utf8"
+    );
+    fileStream.on("open", () => {
+      fileStream.pipe(res);
+    });
+    fileStream.on("error", (err) => {
+      res.end(err);
+    });
+  })
+  .listen(5000);
